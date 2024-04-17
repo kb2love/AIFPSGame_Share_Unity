e2a60356f9f1936d6e940c61a495f38af0ae0810
21acad <162068259+kb2love@users.noreply.github.com>
2024-04-17 23:37:16 +0900

    zzzz

--- Assets/02.Scripts/Player/CameraDirection.cs
+++ Assets/02.Scripts/Player/CameraDirection.cs
@@ -6,21 +6,14 @@ public class CameraDirection : MonoBehaviour
 {
     private Transform thirdCameraTr;                        //3인칭 시점의 카메라의 포지션
     private Transform cameraPivotTr;                        //3인칭 시점의 카메라의 부모 포지션
-    private Animator animator;                              //IK방식을 위한 애니메이터
     private Camera thirdCamera;                             //3인칭 시점의 카메라
     private Camera OnestCamera;                             //1인칭 시점의 카메라
-    private Transform leftHand;                             //IK방식을 위한 왼손
-    private Transform rightHand;                            //IK방식을 위한 오른손
     [SerializeField] [Range(0f, 20f)] float cameraDistance; //3인칭 시점에서의 플레이어와 카메라 사이의 거리
     private float cameraHeight;                             //카메라의 기본높이
     [Range(0f, 1000f)] private float mouseSensivity;        //마우스 감도
     private int playerLayer;                                //플레이어의 레이어
     private Vector3 mouseMove;                              //마우스 움직임
     private bool isView;                                    //1인칭과 3인칭을 구분
-    private float weight = 1.0f;
-    private float ikRotWeight = 1.0f;
-    private float armAngleLimit = 60f;
-    private int aniLayerIdx;
 
     void OnEnable()
     {
@@ -33,11 +26,7 @@ public class CameraDirection : MonoBehaviour
         cameraDistance = 2.5f;
         cameraHeight = 1.5f;
         isView = true;
-        animator = GetComponentInChildren<Animator>();
-        leftHand = GameObject.Find("Lthumb").transform;
-        rightHand = GameObject.Find("Rthumb").transform;
         playerLayer = LayerMask.NameToLayer("Player");
-        aniLayerIdx = animator.GetLayerIndex("Shot&Reload Layer");
     }
     void Update()
     {
@@ -75,16 +64,6 @@ public class CameraDirection : MonoBehaviour
         caracterRot.x = caracterRot.z = 0f;
         transform.rotation = Quaternion.Slerp(transform.rotation, caracterRot, 10f * Time.deltaTime);
     }
-    private void OnAnimatorIK(int layerIdx)
-    {
-        if (layerIdx != aniLayerIdx) return;
-        animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHand.transform.position);
-        animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHand.transform.rotation);
-        animator.SetIKPosition(AvatarIKGoal.RightHand, rightHand.transform.position);
-        animator.SetIKRotation(AvatarIKGoal.RightHand, rightHand.transform.rotation);
-        leftHand.localEulerAngles = mouseMove;  
-        rightHand.localEulerAngles = mouseMove;
-    }
     private void OnestPerson()
     {
         OnestCamera.depth = 1f;
@@ -96,6 +75,7 @@ public class CameraDirection : MonoBehaviour
                                   Input.GetAxisRaw("Mouse X") * mouseSensivity * Time.deltaTime, 0f);
         mouseMove.x = Mathf.Clamp(mouseMove.x, -40f, 40f);
         Vector3 Rot = mouseMove;
+        Rot.z = Rot.x = 0;
         transform.localEulerAngles = Rot;
     }
 }
